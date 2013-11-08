b5b6398afc2918b29cdc0e273c113c0fcbf7642f
marek-g <marek-g@user>
2013-11-08 20:10:16 +0100

    Initil version from Tolino Shine eBook reader.

---
+++ README.md
@@ -0,0 +1,1 @@
+Original kernel from Tolino Shine - Andorid 2.3 eInk tablet which was found hardware compatible with Kobo Glo.
---
+++ set_env.sh
@@ -0,0 +1,3 @@
+export ARCH=arm
+export CROSS_COMPILE=/home/work/myandroid/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export PATH=$PATH:/home/work/working/mx50_android_uboot_imx/tools/
